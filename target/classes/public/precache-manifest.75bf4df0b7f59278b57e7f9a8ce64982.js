309d4b1a35aadfdab11551d7b46f5ba9607c103f
Qiru Hu <qiru@cs.ucsb.edu>
2021-01-20 14:19:29 -0500

    Try to deploy again

---
+++ target/classes/public/precache-manifest.75bf4df0b7f59278b57e7f9a8ce64982.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecddbdaabdfa37d3c1218420902f2195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e7a4e76aeeeb625c1c",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "32ea0a8bb4f57bd0ca1d",
+    "url": "/static/css/main.57257572.chunk.css"
+  },
+  {
+    "revision": "46e7a4e76aeeeb625c1c",
+    "url": "/static/js/2.9ad4c965.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.9ad4c965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ea0a8bb4f57bd0ca1d",
+    "url": "/static/js/main.219fec5e.chunk.js"
+  },
+  {
+    "revision": "f0893082a65889490834",
+    "url": "/static/js/runtime-main.1cf212e2.js"
+  },
+  {
+    "revision": "6c1dc7cb20a0d415c831d8f308bf448a",
+    "url": "/static/media/checkbox-checked.6c1dc7cb.svg"
+  },
+  {
+    "revision": "7cbb5a769011186c0606362b27034bfb",
+    "url": "/static/media/checkbox.7cbb5a76.svg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  }
+]);
